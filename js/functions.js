cf4245b0e10e3dc0711a776f2b0a0882fe0e8fc1
Henry <projectsoricidae@gmail.com>
2016-07-22 07:44:42 -0700

    fading logo

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,10 @@ $(window).scroll(function(){
 		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
 	});
 
+	$('.logo').css(
+		'opacity', '1' - wScroll/600
+	);
+
 	$('.dragonfly').css({
 		'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
 	});
